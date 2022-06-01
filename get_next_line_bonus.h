b125475e8a2d57d2be5b3cce9123abff31f5d22e
Seongmin Sim <sesim@c4r1s1.42seoul.kr>
2022-06-01 13:45:14 +0900

    bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: sesim <sesim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 12:39:01 by sesim             #+#    #+#             */
-/*   Updated: 2022/05/30 15:51:16 by sesim            ###   ########.fr       */
+/*   Updated: 2022/06/01 12:37:15 by sesim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,28 @@
 # define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
+# include <stdlib.h>
 
-typedef struct	s_list
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+typedef struct s_list
 {
-	int					fd;
-	char				*bac;
-	struct s_gnl_list	*next;
-	struct s_gnl_list	*prev;
+	char			*line;
+	int				idx;
+	struct s_list	*next;
+	struct s_list	*prev;
 }	t_list;
 
-size_t	ft_strlen(const char *s);
-char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
+char	*ft_strjoin(char *s1, char *s2);
+char	*new_line(char *bac);
+char	*read_line(int fd, char *bac);
+char	*get_line(char *bac);
+char	*get_next_line(int fd);
+t_list	*lst_new(t_list *head, int fd);
 
 #endif
